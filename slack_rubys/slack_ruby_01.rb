f76384bf695515e0e886f7abb0f1a225a67d6686
土井 大平 <daihei.doi@admin-no-iMac-3.local>
2015-12-08 12:10:23 +0900

    Ruby対応 ToDo: - 自宅ローカルPCでの対応

--- slack_rubys/slack_ruby_01.rb
+++ slack_rubys/slack_ruby_01.rb
@@ -1,9 +1,22 @@
-#!/usr/bin/env ruby
-require "slack"
+# !/usr/bin/env ruby
+require 'slack'
 
 Slack.configure do |config|
 	config.token = 'xoxp-15751162311-15745376292-16149053830-83d5f684fd'
 end
 
-Slack.chat_postMessage(text: "TEST!", channel: 'daiheidoi_alim_task')
+Slack.chat_postMessage(text: "TEST!", channel: '#daiheidoi_alim_task')
 
+#require 'slack'
+#Slack.configure {|config| config.token = TOKEN }
+#client = Slack.realtime
+
+#client.on :hello do
+#  puts 'Successfully connected.'
+#end
+
+#client.on :message do |data|
+  # respond to messages
+#end
+
+#client.start
