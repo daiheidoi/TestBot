d95521250ce7f08fd3b220418f85b1a840956b39
土井 大平 <daihei.doi@admin-no-iMac-3.local>
2015-12-10 12:40:53 +0900

    ファイルアクセス監視byPython

---
+++ slack_pythons/slack_test_post.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+
+import urllib
+import urllib2
+import sys
+
+url = "https://slack.com/api/chat.postMessage"
+
+params = {'token':'xoxp-15751162311-15745376292-16149053830-83d5f684fd',   # トークン
+          'channel':'C0FT2FNUE', # チャンネルID
+          'text': ''    # 送信するテキスト
+}
+
+params['text'] = '僕が ' + sys.argv[2] +  ' ファイルは' + sys.argv[1] + 'だ！！'
+params = urllib.urlencode(params)
+
+req = urllib2.Request(url)
+# ヘッダ追加
+req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+# パラメータ追加
+req.add_data(params)
+
+res = urllib2.urlopen(req)
+
+# レスポンス取得
+body = res.read()
