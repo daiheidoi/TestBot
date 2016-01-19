d530db275dfaa048ec3fde8c804863b9b758175c
土井 大平 <daihei.doi@admin-no-iMac-3.local>
2016-01-19 15:14:05 +0900

    美人対応

---
+++ scripts/bijin.coffee
@@ -0,0 +1,28 @@
+# Description:
+#   Returns 美人
+#
+#
+# Commands:
+#   hubot bijin - 美人を返します
+#
+# Author:
+#   
+
+arrify = require('arrify')
+dateFormat = require('dateformat')
+
+weathearAreaList = require('../config/weather_area_list.json')
+
+module.exports = (robot) ->
+  robot.respond /美人/i, (msg) ->
+    msg
+      .http('http://bjin.me/api/?type=rand&count=1&format=json')
+      .header('Accept', 'application/json')
+      .get() (err, res, body) ->
+        if err
+          msg.send('美人の取得に失敗しました')
+          return
+        result = JSON.parse(body)
+        msg.send "お名前: #{result.category}\n" +
+        "#{result.thumb}\n" +
+        "link: #{result.link}"
--- slack_rubys/slack_time_signal.rb
+++ slack_rubys/slack_time_signal.rb
@@ -7,16 +7,16 @@ Slack.configure do |config|
 end
 
 text = case Time.now.hour
-       when 10 then '10時: 私の戦闘力は53万です'
-       when 11 then '11時: みんなって誰だよ・・・・・・。かーちゃんに『みんな持ってるよぉ！』って物ねだるときに言うみんなかよ・・・・・・。誰だよそいつら・・・・・・。友達いないからそんな言い訳使えたことねぇよ・・・・・・ '
-       when 12 then '12時: そりゃ悪手だろ蟻んコ'
-       when 13 then '13時: おめーも死ぬまで死ぬなよ'
-       when 14 then '14時: みんながぼっちになれば争いも揉め事も起きないだろ'
-       when 15 then '15時: 趣味でヒーローをやっているものだ'
-       when 16 then '16時: 死ねとか殺すとか軽々しく言うんじゃねぇよ。ぶっ殺すぞ。'
-       when 17 then '17時: いやまて・・・俺は悪くない、社会が悪い。'
-       when 18 then '18時: 女子の言う『いい人』とは１００％どうでもいい人の事である。'
-       when 19 then '19時: 帰るか'
+       when 10 then 'bing image 美人'
+       when 11 then 'bing image 美人'
+       when 12 then 'bing image 美人'
+       when 13 then 'bing image 美人'
+       when 14 then 'bing image 美人'
+       when 15 then 'bing image 美人'
+       when 16 then 'bing image 美人'
+       when 17 then 'bing image 美人'
+       when 18 then 'bing image 美人'
+       when 19 then 'bing image 美人'
        end
 
 Slack.chat_postMessage(text: text, channel: '#smoking-area', username: 'ruby_bot' )
