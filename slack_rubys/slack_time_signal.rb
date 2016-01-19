01e796f47c7cb463e652f9767f057a2a4c1aca56
土井 大平 <daihei.doi@admin-no-iMac-3.local>
2016-01-19 15:15:52 +0900

    時報対応

--- slack_rubys/slack_time_signal.rb
+++ slack_rubys/slack_time_signal.rb
@@ -7,16 +7,16 @@ Slack.configure do |config|
 end
 
 text = case Time.now.hour
-       when 10 then 'bing image 美人'
-       when 11 then 'bing image 美人'
-       when 12 then 'bing image 美人'
-       when 13 then 'bing image 美人'
-       when 14 then 'bing image 美人'
-       when 15 then 'bing image 美人'
-       when 16 then 'bing image 美人'
-       when 17 then 'bing image 美人'
-       when 18 then 'bing image 美人'
-       when 19 then 'bing image 美人'
+       when 10 then 'bing image 10時'
+       when 11 then 'bing image 11時'
+       when 12 then 'bing image 12時'
+       when 13 then 'bing image 13時'
+       when 14 then 'bing image 14時'
+       when 15 then 'bing image 15時'
+       when 16 then 'bing image 16時'
+       when 17 then 'bing image 17時'
+       when 18 then 'bing image 18時'
+       when 19 then 'bing image 19時'
        end
 
 Slack.chat_postMessage(text: text, channel: '#smoking-area', username: 'ruby_bot' )
